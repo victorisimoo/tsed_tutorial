88d37efced0c58bc33dbf810533c18410628e34b
Victor Hernández <116760982+vhnabenik@users.noreply.github.com>
2022-11-03 14:12:56 -0600

    i add correct test

--- src/controllers/rest/HelloWorldController.spec.ts
+++ src/controllers/rest/HelloWorldController.spec.ts
@@ -11,39 +11,28 @@ describe("HelloWorldController", () => {
     expect(instance).toBeInstanceOf(HelloWorldController);
   });
 
-  it("aFunction should returns hello", () => {
+  it("Function should returns hello", () => {
     const instance = PlatformTest.get<HelloWorldController>(HelloWorldController);
-
     expect(instance.aFunction()).toBe("hello");
   });
 
-  // PRUEBA .TOBE 1
-  it("1 + 1 es 2", () => {
+  it("addition of numbers test", () => {
     const instance = PlatformTest.get<HelloWorldController>(HelloWorldController);
     const firstNumber = 1;
     const secondNumber = 1;
     expect(instance.sumarDosNumeros(firstNumber,secondNumber)).toBe(2);
   });
-  // PRUEBA .TOBE 2
-  it("Dar vuelta a una phrase", () => {
+
+  it("change order phrase", () => {
     const instance = PlatformTest.get<HelloWorldController>(HelloWorldController);
-    const phrase = "hola";
-    expect(instance.turnPhrase(phrase)).toBe("aloh");
+    const phrase = "our";
+    expect(instance.turnPhrase(phrase)).toBe("ruo");
   });
 
-  // SPY ON
-  it("Inspeccionar función", () => {
-    const instance = PlatformTest.get<HelloWorldController>(HelloWorldController);
-    let x = jest.spyOn(instance, "changeCapitalization");
-    instance.invertir("algo");
-    expect(x).toBeCalled();
-  });
-
-  // MOCK
-  it("Inspeccionar función", () => {
+  it("analysis function", () => {
     const instance = PlatformTest.get<HelloWorldController>(HelloWorldController);
     let x = jest.spyOn(instance, "changeCapitalization").mockReturnValueOnce("xD");
-    expect(instance.invertir("laboratorio")).toBe("xD");
-    expect(instance.invertir("laboratorio")).toBe("LABORATORIO");
+    expect(instance.invertir("test")).toBe("asdfasdf");
+    expect(instance.invertir("test")).toBe("TEST");
   });
 });
